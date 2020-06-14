3b87eb18f122322de01e5240248b0f9153c9de50
Dimitar <dimitar.naidenov.98@gmail.com>
2020-06-14 19:53:10 +0300

    fix users button

--- includes/header.php
+++ includes/header.php
@@ -16,49 +16,6 @@ include 'includes/db.php';
 
 <body>
   <div class="container-fluid">
-
-<<<<<<< .mine
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="profile.php">Profile</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="users.php">Users</a>
-      </li>
-    </ul>
-     
-  </div>
-</nav>
-<?php
-session_start();
-include 'includes/db.php';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="#">Alumni</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -94,9 +51,12 @@ include 'includes/db.php';
 
           <?php          }          ?>
 
+          <li class="nav-item">
+            <a class="nav-link" href="users.php">Users</a>
+          </li>
 
         </ul>
 
       </div>
     </nav>
->>>>>>> .theirs
+    <?php
