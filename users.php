480bb6b46eaccd7a9da9b7c37931ab9245d96e0f
Dimitar <dimitar.naidenov.98@gmail.com>
2020-06-15 14:18:24 +0300

    edit user.php anyone can see the one whom there is permission for

--- users.php
+++ users.php
@@ -1,9 +1,26 @@
 <?php
 include 'includes/header.php';
 
-$data = $pdo->query("SELECT first_name, last_name FROM users")->fetchAll();
+//when no user loged in
+$data = $pdo->query("SELECT first_name, last_name , visibility FROM users WHERE visibility = 0")->fetchAll();
 // and somewhere later:
+
 foreach ($data as $row) {
-    echo $row['first_name']." ".$row['last_name']."<br />\n";
+    echo $row['first_name'] . " " . $row['last_name'] . "<br />\n";
 }
-?>
+if (isset($_SESSION["username"])) {
+    $stmt = $pdo->prepare('SELECT * FROM users WHERE username = :username ');
+    $stmt->execute(['username' => $_SESSION['username']]);
+    $user = $stmt->fetch();
+    $user_speciality = $user["speciality"]; 
+    $user_uni_group = $user["uni_group"];
+    
+   // var_dump($user_uni_group);die();
+
+    $data = $pdo->query("SELECT first_name, last_name, uni_group, speciality FROM users WHERE (visibility = 1 AND speciality = '$user_speciality') 
+                                                                                            OR (visibility = 2 AND uni_group = '$user_uni_group')")->fetchAll();
+
+    foreach ($data as $row) {
+            echo $row['first_name'] . " " . $row['last_name'] . "<br />\n";
+    }
+}
