812340b6cf765267ca59d03af437f8711016ff84
Kristina Asenova <krisstina.asenova@gmail.com>
2020-07-06 20:21:13 +0300

    Added basic post form

--- index.php
+++ index.php
@@ -1,13 +1,29 @@
 <?php
 include 'includes/header.php';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Recent posts</title>
+  <link href="css/blog-home.css" rel="stylesheet">
+</head>
+
 <div class="row">
     <div class="col-12">
-       <h1 class="text-center">Reacent posts</h1> 
+       <h1 class="text-center">Recent posts</h1> 
        <div>
-           <div class="text"></div>
-           <div class="time"></div>
-           <div class="from"></div>
+       <div class="card mb-4">
+          <div class="card-body">
+            <h2 class="card-title">Post Title</h2>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+            <a href="#" class="btn btn-primary">Read More &rarr;</a>
+          </div>
+          <div class="card-footer text-muted">
+            Posted on January 1, 2020 by Krisaka
+          </div>
+        </div>
        </div> 
     </div>
 </div>
