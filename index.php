91aab394674bd7fb66e576aee84694f7476e066d
Kristina Asenova <krisstina.asenova@gmail.com>
2020-07-20 23:35:57 +0300

    Added design to index page and login

--- index.php
+++ index.php
@@ -1,83 +1,39 @@
 <?php
 include 'includes/header.php';
-
-
-$stmt = $pdo->query('SELECT messages.* , users.username FROM messages INNER JOIN users ON users.id = messages.user_id ORDER BY messages.created DESC  ');
-$posts = $stmt->fetchAll();
-$numberOfPosts = count($posts);
-$counter = 0;
-
-
-$stmt2 = $pdo->query('SELECT count(id) as count FROM users');
-$numberUsers = $stmt2->fetchAll()[0]['count'];
 ?>
 
-<!-- Page Content -->
-<div class="container">
+<!doctype html>
+<html>
+<head>
+  <link rel="stylesheet" href="styles/index.css">
+  <style>
+      body {
+    background-image: url("styles/images/student.jpg");
+  }
+  </style>
+</head>
 
-    <div class="row">
+<body >
 
-        <!-- Blog Entries Column -->
-        <div class="col-md-12">
-            <h1 class="text-center">Recent posts</h1>
-        </div>
-    </div>
-    <?php
-    foreach ($posts as $post) {
-
-    ?>
-        <div class="row">
-            <div class="col-8">
-                <div>
-                    <div class="card mb-4">
-                        <div class="card-body">
-                            <h2 class="card-title"><?php echo $post['title'] ?></h2>
-                            <p class="card-text"><?php echo $post['message']  ?></p>
-                            <a href="#" class="btn btn-primary">Read comments &rarr;</a>
-                            <a href="#" class="btn btn-primary">Create comments &rarr;</a>
-                        </div>
-                        <div class="card-footer text-muted">
-                            Posted on <?php echo $post['created'] ?> by <?php echo $post['username']  ?>
-                        </div>
-                    </div>
-                </div>
+<div class="masthead">
+    <div class="masthead-bg"></div>
+    <div class="container h-100">
+      <div class="row h-100">
+        <div class="col-12 my-auto">
+          <div class="masthead-content text-white py-5 py-md-0">
+            <h1 class="mb-3">We are stronger together!</h1>
+            <p class="mb-5">Let's continue to be part of this big family!</p>
             </div>
-            <?php
-            if ($counter == 0) {
-            ?>
-                <div class="col-md-4">
-                    <!-- Statistics -->
-                    <div class="card my-4">
-                        <h5 class="card-header">Statistics</h5>
-                        <div class="card-body">
-                            <div>
-                                There are <?php echo $numberUsers ?> members the group
-                            </div>
-                            <div>
-                                There are <?php echo $numberOfPosts ?> posts the group
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            <?php
-            }
-            $counter++;
-            ?>
+          </div>
         </div>
-    <?php } ?>
-    <!-- Pagination -->
-    <ul class="pagination justify-content-center mb-4">
-        <li class="page-item">
-            <a class="page-link" href="#">&larr; Older</a>
-        </li>
-        <li class="page-item disabled">
-            <a class="page-link" href="#">Newer &rarr;</a>
-        </li>
-    </ul>
-</div>
+      </div>
+    </div>
+  </div>
 
+    
 
+</body>
 
-</div>
+</html>
 <?php
 include 'includes/footer.php';
--- login.php
+++ login.php
@@ -25,34 +25,49 @@ if (isset($_POST['login'])) {
 }
 ?>
 
+<!doctype html>
+<html>
+<head>
+  <link rel="stylesheet" href="styles/login.css">
+  <style>
+      .bg-image {
+        background-image: url('styles/images/connections_people.png');
+        background-size: cover;
+        background-position: center;
+}
+  </style>
+</head>
+
 <body>
-    <div class="row card-body" style="margin-top:8%">
-        <div class="col-md-12">
-            <section class="col-md-12">
-                <form id="account" method="post">
-                    <h2>Welcome to Alumni-club</h2>
-                    <hr />
-                    <div class="form-group">
-                        <label>Username</label>
-                        <br>
-                        <input type="text" name="username" class="input">
-                    </div>
 
-                    <div class="form-group">
-                        <label>Password</label>
-                        <br>
-                        <input type="password" name="password" class="input">
-                    </div>
+<div class="container-fluid">
+  <div class="row no-gutter">
+    <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+    <div class="col-md-8 col-lg-6">
+      <div class="login d-flex align-items-center py-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-9 col-lg-8 mx-auto">
+              <h3 class="login-heading mb-4">Welcome to the Alumni club!</h3>
+              <form id="account" method="post">
+                <div class="form-label-group">
+                  <input  type="text" name="username" class="form-control" placeholder="Username" required autofocus>
+                </div>
 
-                    <div class="form-group">
-                        <button type="submit" class="button" name="login" id="login-button">Login</button>
-                    </div>
-                </form>
-            </section>
+                <div class="form-label-group">
+                  <input type="password" name="password" class="form-control" placeholder="Password" required>
+                </div>
+
+                <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" name="login" id="login-button" type="submit">Sign in</button>
+               
+              </form>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-    </div>
-    </div>
+  </div>
+</div>
 </body>
 
 <?php
